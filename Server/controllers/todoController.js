620829a66ad37393911746af0e80ec374c5c85a1
MEHMETTOKGOZ <42mt06@gmail.com>
2023-01-10 22:12:55 +0300

    .

--- Server/controllers/todoController.js
+++ Server/controllers/todoController.js
@@ -1,39 +1,49 @@
-const Todo = require("../modules/todoItems")
+const Todo = require("../modules/todoItems");
 
 //CREATE NEW TODO
 
 const createNewTodo = async (req, res) => {
-    let newTodo = new Todo({
-        item: req.body.item
-    });
-    await newTodo.save();
-    res.send({ message: "Item Added"
-    });
+  let newTodo = new Todo({
+    item: req.body.item,
+  });
+  await newTodo.save();
+  res.send({ message: "Item Added" });
 };
-
 
 //GET ALL TODOS
 
 const getTodos = async (req, res) => {
-    let todos = await Todo.find({})
-    res.send(todos);
+  let todos = await Todo.find({});
+  res.send(todos);
 };
 
 //UPDATE A TODO
 
-const updateTodo = async (req, res) => {try {
-    const updateItem = await Todo.findByIdAndUpdate(req.params.id, {$set: req.body});
-    res.send({message: "Item updated"})
-} catch (error) {
-    res.send(error)
-}
+const updateTodo = async (req, res) => {
+  try {
+    const updateItem = await Todo.findByIdAndUpdate(req.params.id, {
+      $set: req.body,
+    });
+    res.send({ message: "Item updated" });
+  } catch (error) {
+    res.send(error);
+  }
 };
 
+//DELETE TODO
 
-
+const deleteTodo = async (req, res) => {
+  try {
+    let deleteItem = await Todo.findByIdAndDelete(req.params.id)
+    res.send({message: "Item deleted"});
+  } catch (error) {
+    res.send(error);
+  }
+};
 
 module.exports = {
-    createNewTodo,
-    getTodos,
-    updateTodo
+  createNewTodo,
+  getTodos,
+  updateTodo,
+  deleteTodo,
 };
--- Server/routes/todoItemRoute.js
+++ Server/routes/todoItemRoute.js
@@ -5,5 +5,6 @@ const router = require("express").Router();
 router.post("/create", todoControl.createNewTodo);
 router.get("/", todoControl.getTodos);
 router.put("/update/:id", todoControl.updateTodo);
+router.delete("/delete/:id", todoControl.deleteTodo)
 
 module.exports = router;
