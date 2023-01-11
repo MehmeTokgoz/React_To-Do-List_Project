4eff8ed135b87e78ef5eeed16b0b23e79e23d146
MEHMETTOKGOZ <42mt06@gmail.com>
2023-01-11 13:37:26 +0300

    Frontend updated

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
+import axios from "axios"
 import "./App.scss";
 
 function App() {
+const [itemText, setItemText] = useState("");
+// ADD NEW TO-DO TO DATABASE
+const addItem = async () => {
+  e.prevent.default();
+try {
+  const res = await axios.post("http://localhost:3003/todos/create", {item: itemText})
+
+} catch (error) {
+  console.log(error)
+}
+}
+
+
   return (
     <div className="App">
-      <form>
-        <input type="text" placeholder="Add Todo" />
+      <h1> Todo List </h1>
+      <form className="form" onSubmit = {(e) => {addItem(e)}}>
+        <input type="text" placeholder="Add Todo" onChange={(e)=>{setItemText(e.target.value)}} value={itemText}/>
         <button type="submit"> Add </button>
       </form>
-      <div className="todoListItems">
+      <div className="todo-listItems">
         <div className="todo-item">
           <p className="item-content"> First todo </p>
           <button className="update-item">Update</button>
