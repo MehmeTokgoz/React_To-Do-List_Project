f71d0f3fae825fb127db7d2ce5e93310c18d9d78
MEHMETTOKGOZ <42mt06@gmail.com>
2023-01-12 22:35:52 +0300

    Completed

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,7 +5,8 @@ import "./App.scss";
 function App() {
   const [itemText, setItemText] = useState("");
   const [listItems, setListItems] = useState([]);
-  const [deletedItem, setDeletedItem] = useState("");
+  const [updating, setUpdating] = useState("");
+  const [updateitemText, setupdateitemText] = useState("");
 
   // ADD NEW TO-DO TO DATABASE
   const addItem = async (e) => {
@@ -14,8 +15,8 @@ function App() {
       const res = await axios.post("http://localhost:3003/todos/create", {
         item: itemText,
       });
-      setListItems((prev) => [...prev, res.data]);
       setItemText("");
+      setListItems((prev) => [...prev, res.data]);
     } catch (error) {
       console.log(error);
     }
@@ -28,7 +29,6 @@ function App() {
       try {
         const res = await axios.get("http://localhost:3003/todos");
         setListItems(res.data);
-        console.log("render");
       } catch (error) {
         console.log(error);
       }
@@ -43,24 +43,61 @@ function App() {
       const delet = await axios.delete(
         `http://localhost:3003/todos/delete/${id}`
       );
-      const newList = listItems.filter(items =>
-        items._id !== id
-      );
+      const newList = listItems.filter((items) => items._id !== id);
       setListItems(newList);
     } catch (error) {
       console.log(error);
     }
   };
 
+  // UPDATE-TO-DO
+  const updateItem = async (e) => {
+    e.preventDefault();
+    try {
+      const updatingElement = await axios.put(
+        `http://localhost:3003/todos/update/${updating}`,
+        { item: updateitemText }
+      );
+      setupdateitemText("");
+      setUpdating("");
+      console.log(updatingElement.data);
+      const updatedElementIndex = listItems.findIndex(
+        (item) => item._id === updating
+      );
+      const updatedElement = (listItems[updatedElementIndex].item =
+        updateitemText);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // UPDATE-FORM
+
+  const updateForm = () => (
+    <form
+      className="update-form"
+      onSubmit={(e) => {
+        updateItem(e);
+      }}
+    >
+      <input
+        className="update-new-input"
+        type="text"
+        placeholder="Update to-do"
+        onChange={(e) => {
+          setupdateitemText(e.target.value);
+        }}
+        value={updateitemText}
+      />
+      <button className="update-new-btn" type="submit">
+        Update
+      </button>
+    </form>
+  );
+
   return (
     <div className="App">
       <h1> Todo List </h1>
-      <form
-        className="form"
-        onSubmit={(e) => {
-          addItem(e);
-        }}
-      >
+      <form className="form" onSubmit={(e) => addItem(e)}>
         <input
           type="text"
           placeholder="Add Todo"
@@ -72,22 +109,33 @@ function App() {
         <button type="submit"> Add </button>
       </form>
       <div className="todo-listItems">
-        {listItems.map((item) => {
-          return (
-            <div className="todo-item">
-              <p className="item-content"> {item.item} </p>
-              <button className="update-item">Update</button>
-              <button
-                className="delete-item"
-                onClick={() => {
-                  deleteItem(item._id);
-                }}
-              >
-                Delete
-              </button>
-            </div>
-          );
-        })}
+        {listItems.map((item) => (
+          <div className="todo-item">
+            {updating === item._id ? (
+              updateForm()
+            ) : (
+              <>
+                <p className="item-content"> {item.item} </p>
+                <button
+                  className="update-item"
+                  onClick={() => {
+                    setUpdating(item._id);
+                  }}
+                >
+                  Update
+                </button>
+                <button
+                  className="delete-item"
+                  onClick={() => {
+                    deleteItem(item._id);
+                  }}
+                >
+                  Delete
+                </button>
+              </>
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
